2ba6b20faef593b7ec7c648d376163ca09baed10
JulianRenaud <midway6220@gmail.com>
2022-10-17 12:50:07 -0500

    First Commit

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1, 2, 3, 4, & 5
+const firstName = "Julian";
+let lastName = "Renaud";
+var age = 13;
 
-// Exercise 2
+let language = "JavaScript";
+let createdYear = 1995;
+let isCasSensitive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = false;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
